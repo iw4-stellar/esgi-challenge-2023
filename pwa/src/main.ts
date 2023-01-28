c9ece94040917b467ea1bf215b456134af541b1d
Rudro Khan <rudrokhan.pro@gmail.com>
2023-01-28 13:06:02 +0100

    add of axios

--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { i18n } from './plugins/i18n'
 import { router } from './plugins/router'
+import { axios } from './plugins/axios'
 import './style.css'
 import App from './App.vue'
 
@@ -8,5 +9,7 @@ const app = createApp(App)
 
 app.use(i18n)
 app.use(router)
+
+app.config.globalProperties.$api = axios;
 
 app.mount('#app')
--- pwa/src/vite-env.d.ts
+++ pwa/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+import type { AxiosInstance } from "axios";
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $api: AxiosInstance
+  }
+}
