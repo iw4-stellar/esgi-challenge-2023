5b8d63a1f9a1f752cd7649d39d131dbaf8b34b05
Rudro Khan <rudrokhan.pro@gmail.com>
2023-01-28 13:19:56 +0100

    add of pinia

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAppStore } from './store/app';
+
+const store = useAppStore()
+
+const { name } = store;
+
+</script>
 
 <template>
   <div class="app">
+    <h1>{{ name }}</h1>
     <router-view></router-view>
   </div>
 </template>
--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { i18n } from './plugins/i18n'
 import { router } from './plugins/router'
 import { axios } from './plugins/axios'
+import { pinia } from './plugins/pinia'
 import './style.css'
 import App from './App.vue'
 
@@ -9,6 +10,7 @@ const app = createApp(App)
 
 app.use(i18n)
 app.use(router)
+app.use(pinia)
 
 app.config.globalProperties.$api = axios;
 
